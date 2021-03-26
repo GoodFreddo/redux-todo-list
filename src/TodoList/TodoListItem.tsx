4f8c5c1f2bafd1470daae2e94745bc1df14e1bb2
Lachlan <goodfreddo@yahoo.co.uk>
2021-03-26 16:59:47 +0000

    Initial commit

---
+++ src/TodoList/TodoListItem.tsx
@@ -0,0 +1,5 @@
+export interface TodoListItem {
+  title: string;
+  text: string;
+  order: number;
+}
---
+++ src/TodoList/state/TodoListAction.ts
@@ -0,0 +1,4 @@
+import { Console } from "console";
+import { write } from "fs";
+
+export function doNothing(){};
